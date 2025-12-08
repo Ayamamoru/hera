a1f1cdca519348d4cf8faccce4b8d551bd6eade7
nomopotatoes <hippogif04@gmail.com>
2025-12-08 19:52:44 +0000

    test commit to github

--- app/page.tsx
+++ app/page.tsx
@@ -17,19 +17,20 @@ export default function Home() {
           </div>
         </div>
       </nav>
-
+      {/*Center page content!! */}
       <main className="flex-1 flex flex-col items-center justify-center w-full overflow-auto gap-4">
         <p className="text-lg text-zinc-600 dark:text-zinc-400">Building the Future,</p>
         <p className="text-lg text-zinc-600 dark:text-zinc-400">One Kernel at a Time</p>
         <p className="text-sm text-zinc-400 dark:text-zinc-400">A city of Ottawa Premier Vex Robotics Team 2381</p>
       </main>
 
+      {/* Title, OVER the navbar border */}
       <div className="fixed top-16 left-1/2 -translate-x-1/2 -translate-y-1/2 z-50 flex flex-col items-center gap-6 text-center">
         <h1 className="text-7xl font-bold leading-none tracking-tight text-black dark:text-zinc-50">
           Kernel Bye
         </h1>
       </div>
-
+      {/* That oval thing in the figma... Under heading */}
       <div className="fixed top-48 left-1/2 -translate-x-1/2 z-50">
         <div className="px-4 py-2 rounded-full bg-zinc-900 dark:bg-zinc-100 text-white dark:text-black text-sm font-medium">
           2381 robotics
@@ -38,3 +39,5 @@ export default function Home() {
     </div>
   );
 }
+
+//Yo am i crazy or is this not committing to github
